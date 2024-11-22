cf3a33526f63e3625034cf1c8009ba0efc08f357
Armin Karimi <157640791+arminak6@users.noreply.github.com>
2024-11-22 12:05:31 +0100

    structure of the project

---
+++ assignment_1/src/node_a.cpp
@@ -0,0 +1,15 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Int32MultiArray.h"
+#include "assignment_1/ApriltagsIdS.h"
+
+
+
+
+
+int main(int argc, char **argv)
+{
+
+
+    return 0;
+}
